272857973b2827521ebd8270d0224c6bcb78a7c0
Daniel Kirch <daniel.kirch@2trde.com>
2020-06-08 17:06:20 +0200

    rm httpotion app, project was not compiling

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Excrawl.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :httpotion]]
+    []
   end
 
   # Dependencies can be Hex packages:
